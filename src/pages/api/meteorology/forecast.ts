a35d1ee9b39357eb1d5a718cc5afe67889dbd344
Marco Porto <50577988+4SMarcoPorto@users.noreply.github.com>
2021-02-01 18:34:24 +0000

    Done meteorology endpoints

--- src/pages/api/meteorology/forecast.ts
+++ src/pages/api/meteorology/forecast.ts
@@ -1,13 +1,39 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-function forecast(request: NextApiRequest,response: NextApiResponse){
-    const dynamicDate = new Date();
+export default async function forecast(request: NextApiRequest,response: NextApiResponse){
+    //Check if request.method === GET (prevent none GET methods to this endpoint)
+    if(request.method === 'GET'){
+        if(request.body.localCode != undefined && request.body.localCode != null && request.body.localCode != '' ){
+            //try catch to handle json unexpected token exception (if throw, probably location code doesnt exist and IPMA API rout, redirect to IPMA home page)
+            try{
+                //Fetch ipma forecast for a given location code
+                const ipmaForecastResponse = await fetch(`https://api.ipma.pt/public-data/forecast/aggregate/${request.body.localCode}.json`);
+                const ipmaForecastResponseJson = await ipmaForecastResponse.json();
+                
+                //Search for location provide on GET
+                let ipmaForecast = [];
+                ipmaForecastResponseJson.map(obj =>  {
+                    if(obj.idPeriodo == 24)
+                        ipmaForecast.push(obj);
+                });
 
-    response.setHeader('Cache-Control','s-maxage=10,stale-while-revalidate');
-    
-    response.json({
-        dataISO:dynamicDate.toISOString(),
-        dataUTC:dynamicDate.toUTCString()
-    });
+                //Define 12 hours of response cache (43200 seconds)
+                response.setHeader('Cache-Control','s-maxage=43200,stale-while-revalidate'); 
+
+                //Before return ipmaLocation check if is != empty (true => return location | false => return 404 status)
+                if(ipmaForecast.length != 0){
+                    response.json(ipmaForecast);
+                }else{    
+                    response.status(404).json({code:'Not Found'});
+                }
+            }catch(exception){
+                response.status(404).json({code:'Not Found'});
+            }
+        }else{
+            response.status(400).json({code:'Bad Request'}); //return 400 http code if request.body.localCode doesn't exist && == null && == empty
+        }
+    }else{
+        response.status(405).json({code:'Method Not Allowed'}); //return 405 http code if request.method !== GET
+    }
 }
-export default forecast;
+
--- src/pages/api/meteorology/zones.ts
+++ src/pages/api/meteorology/zones.ts
@@ -1,33 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import ipmaLocationsResponseJson from './data-temp/meteorology-locations.json'
-
 export default async function zones(request: NextApiRequest,response: NextApiResponse){
     //Check if request.method === GET (prevent none GET methods to this endpoint)
-    if(request.method === 'GET')
-        if(request.body.local != undefined && request.body.local != null && request.body.local != '' ){
+    if(request.method === 'GET'){
+        if((request.body.local != undefined && request.body.local != null && request.body.local != '' ) && (request.body.filterBy != undefined && request.body.filterBy != null && request.body.filterBy != '' )){
             //Fetch ipma locations
-            /*const ipmaLocationsResponse = await fetch('https://api.ipma.pt/public-data/forecast/locations.json');
-            const ipmaLocationsResponseJson = await ipmaLocationsResponse.json();*/
+            const ipmaLocationsResponse = await fetch('https://api.ipma.pt/public-data/forecast/locations.json');
+            const ipmaLocationsResponseJson = await ipmaLocationsResponse.json();
 
             //Search for location provide on GET
             let ipmaLocation = [];
-            ipmaLocationsResponseJson.map(obj =>  {
-                if(obj.local.toLowerCase() == request.body.local.toLowerCase())
-                    ipmaLocation.push(obj);
-            });
 
-            //Define 10 seconds response cache
-            response.setHeader('Cache-Control','s-maxage=10,stale-while-revalidate');
+            if(request.body.filterBy == 'code'){
+                ipmaLocationsResponseJson.map(obj =>  {
+                    if(obj.globalIdLocal == request.body.local)
+                        ipmaLocation.push(obj);
+                });
+            }else if(request.body.filterBy == 'name'){
+                ipmaLocationsResponseJson.map(obj =>  {
+                    if(obj.local.toLowerCase() == request.body.local.toLowerCase())
+                        ipmaLocation.push(obj);
+                });
+            }
+            //Define 7 days of response cache (604800 seconds)
+            response.setHeader('Cache-Control','s-maxage=604800,stale-while-revalidate'); 
 
             //Before return ipmaLocation check if is != empty (true => return location | false => return 404 status)
-            if(ipmaLocation.length != 0)
+            if(ipmaLocation.length != 0){
                 response.json(ipmaLocation);
-            else    
+            }else{
                 response.status(404).json({code:'Not Found'});
-        }else
-            response.status(400).json({code:'Bad Request'}); //return 400 http code if request.body.local doesn't exist && == null && == empty
-    else
+            }
+        }else{
+            response.status(400).json({code:'Bad Request'}); //return 400 http code if request.body.local && request.body.filterBy doesn't exist && == null && == empty
+        }
+    }else{
         response.status(405).json({code:'Method Not Allowed'}); //return 405 http code if request.method !== GET
+    }
 }
-
